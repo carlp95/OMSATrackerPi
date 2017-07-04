565040da131ad5fee9b3e6e88e54dfa164804f13
Neury <20110921@ce.pucmm.edu.do>
2017-07-04 11:39:26 -0400

    Modulo GPS latitude

--- RestAPI.py
+++ RestAPI.py
@@ -4,6 +4,6 @@ from Entidades import Coordenada
 class RestAPI:
     url = "http://httpbin.org/post"
 
-    def postUbicacion(self, coordenada, hora  ):
+    def postUbicacion(self, coordenada, hora ):
         response = unirest.post(self.url, headers={"Accept": "application/json"},
                                 params={"coordenada": coordenada, "hora": hora})
