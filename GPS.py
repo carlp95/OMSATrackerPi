86b4256894c58701a4b8e6611fde4dd7933942fa
Neury <20110921@ce.pucmm.edu.do>
2017-07-10 16:05:24 -0400

    HOTFIX GPS

--- GPS.py
+++ GPS.py
@@ -1,10 +1,8 @@
 import gps
-import time
 
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-
 
 while True:
     try:
@@ -12,14 +10,9 @@ while True:
         # Wait for a 'TPV' report and display the current time
         # To see all report data, uncomment the line below
         # print report
-
         if report['class'] == 'TPV':
-            if (hasattr(report, 'lat') and hasattr(report, 'lon') and hasattr(report, 'time')):
-                print report.lat
-                print report.lon
+            if hasattr(report, 'time'):
                 print report.time
-
-
     except KeyError:
         pass
     except KeyboardInterrupt:
