a8064a86fd9023b4e4b54a5e92855580295cc84b
anyderre <ridoreda1992@gmail.com>
2017-07-18 10:36:14 -0400

    changing coordinate

--- Entidades/Coordenada.py
+++ Entidades/Coordenada.py
@@ -1,9 +1,22 @@
 class Coordenada:
     def __init__(self):
         pass
+
     def __init__(self, longitud, latitud):
         self.longitud = longitud
         self.latitud = latitud
+
+    def __get__(self, instance, owner):
+        return self.longitud
+
+    def __set__(self, instance, value):
+        self.longitud= value
+
+    def __get__(self, instance, owner):
+        return self.latitud
+
+    def __set__(self, instance, value):
+        self.latitud = value
 
     def __unicode__(self):
         return self.latitud + "," + self.longitud
